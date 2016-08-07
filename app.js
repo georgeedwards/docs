5a9f275e52409474908bed1c15e6d0db060be581
George Edwards <g.edwards@gas-sense.co.uk>
2016-08-07 18:26:43 +0100

    angular works!

--- app.js
+++ app.js
@@ -21,6 +21,16 @@ app.set('view engine', 'html');
 
 app.use(express.static('views'));
 app.use(express.static('public'));
+/*
+app.use('/scripts', express.static(__dirname + '/node_modules/core-js/client'));
+app.use('/scripts', express.static(__dirname + '/node_modules/zone.js/dist'));
+app.use('/scripts', express.static(__dirname + '/node_modules/reflect-metadata'));
+app.use('/scripts', express.static(__dirname + '/node_modules/systemjs/dist')); */
+
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
+app.use('/app', express.static(__dirname + '/views/app'));
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -28,14 +38,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 //app.use(serveStatic('docs/public', {'index': ['index.html', 'index.htm']}))
 
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -46,7 +55,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -57,7 +66,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
