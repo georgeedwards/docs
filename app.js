afa4ca47b0902b8b79f01e5fba4943e92f8e1f94
George Edwards <g.edwards@gas-sense.co.uk>
2016-08-07 13:37:09 +0100

    html express

--- app.js
+++ app.js
@@ -12,8 +12,13 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
+/*
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+*/
+app.set('views', path.join(__dirname, '/views'));
+app.engine('html', require('jade').renderFile);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,11 +28,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(serveStatic('docs/public', {'index': ['index.html', 'index.htm']}))
+//app.use(serveStatic('docs/public', {'index': ['index.html', 'index.htm']}))
 
 app.use('/', routes);
-app.use('/users', users);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
