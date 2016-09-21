b55556f58a1bfc02eb8364014844639af0668d5b
George Edwards <g.edwards@gas-sense.co.uk>
2016-09-21 23:05:24 +0100

    plugins API protected and implemented

--- app.js
+++ app.js
@@ -75,9 +75,9 @@ var jwtCheck = jwt({
   audience: 'gZ27aPXK1cCU0j4bauKiTZM5QFC8y9HO'
 });
 
+app.use('/api/path-you-want-to-protect', jwtCheck);
 app.use('/api/path-you-want-to-protect', plugin);
-app.use('/api/secured/plugins', jwtCheck);
-//app.use('/api/plugins', plugin);
+
 
 // Use native Node promises
 mongoose.Promise = global.Promise;
