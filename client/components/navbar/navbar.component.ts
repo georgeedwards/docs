dccf2164901fcbafc88e139a841cc8c96b7e2feb
George <g.edwards@gas-sense.co.uk>
2016-10-09 16:51:25 +0100

    Removed Navbar

--- client/components/navbar/navbar.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'navbar',
-  templateUrl: 'app/navbar/navbar.html',
-  styleUrls: ['app/navbar/navbar.css']
-})
-export class navbarComponent { }
